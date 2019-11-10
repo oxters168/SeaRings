4faa4c4d22dafd67053ac6df918a50084e87bd15
Oxters Wyzgowski <oxters168@gmail.com>
2019-11-10 23:07:27 +0200

    Added gitignore and gitattributes files. Imported Obi Fluid asset. Set up scene. Created gyro script.

---
+++ Assets/Resources/Scripts/GyroRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GyroRotator : MonoBehaviour
+{
+    public Rigidbody target;
+
+    private void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+    private void FixedUpdate()
+    {
+        Quaternion deviceRotation = Input.gyro.attitude;
+        target.MoveRotation(deviceRotation);
+    }
+}
